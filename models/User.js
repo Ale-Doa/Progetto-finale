cccc937b35da9b4616d76933ef8d7293e1262b8a
Ale-Doa <doatialessandro19@gmail.com>
2025-02-03 05:20:48 +0100

    aggiurnata logica prenotazioni

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const Booking = require('./Bookings')
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
@@ -14,6 +15,22 @@ userSchema.pre('save', async function(next) {
     this.password = await bcrypt.hash(this.password, 10);
   }
   next();
+});
+
+// Middleware post-save per cancellare le prenotazioni se l'abbonamento diventa "basic"
+userSchema.post('save', async function (doc, next) {
+  try {
+    const previousMembershipType = this.$original.membershipType; // Ottieni il vecchio valore
+    const currentMembershipType = doc.membershipType;
+
+    // Se l'abbonamento passa da premium a basic, cancella le prenotazioni
+    if (['premium1', 'premium3', 'premium6', 'premium12'].includes(previousMembershipType) && currentMembershipType === 'basic') {
+      await Booking.deleteMany({ user: doc._id });
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = mongoose.model('User', userSchema);
